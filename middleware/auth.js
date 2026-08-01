35c387af35d61b5e5339ee41e31868c74f472db0
Vinayaka Kolur <vinayakakolur@gmail.com>
2026-08-01 20:14:19 +0530

    code done auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const admin = require("../firebase/firebase_admin");
+const { auth } = require("../firebase/firebase_admin");
 
 const authenticateUser = async (req, res, next) => {
   try {
@@ -12,13 +12,13 @@ const authenticateUser = async (req, res, next) => {
 
     const token = authHeader.split("Bearer ")[1];
 
-    const decodedToken = await admin.auth().verifyIdToken(token);
+    const decodedToken = await auth.verifyIdToken(token);
 
     req.user = decodedToken;
 
     next();
   } catch (error) {
-    console.error(error);
+    console.error("Token verification failed:", error);
 
     return res.status(401).json({
       error: "Invalid authentication token",

9ebe82d454807679f43129791eed1cfc8dcf9368
Vinayaka Kolur <vinayakakolur@gmail.com>
2026-08-01 20:04:25 +0530

    made authenticaiton added secret file in render

--- firebase/firebase_admin.js
+++ firebase/firebase_admin.js
@@ -1,12 +1,11 @@
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getAuth } = require("firebase-admin/auth");
+const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("/etc/secrets/serviceAccountKey.json");
 
-initializeApp({
-  credential: cert(serviceAccount),
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = {
-  auth: getAuth(),
+  auth: admin.auth(),
 };
